63f3a7cf808070e6b273e6530f64361445191314
sukeynauysal <sukeynauysal05@gmail.com>
2024-12-23 11:46:49 +0300

    Add files via upload

---
+++ 241223/ornek1.py
@@ -0,0 +1,4 @@
+su_birimi=20
+su_metre_mikrari=int(input("suyun metre küp miktarını girin"))
+su=su_birimi*su_metre_mikrari
+print("ödenecek tutar" ,su)
---
+++ 241223/ornek3.py
@@ -0,0 +1,10 @@
+gun1=int(input("gun1 okudunuz sayfa sayısı "))
+gun2=int(input("gun1 okudunuz sayfa sayısı "))
+gun3=int(input("gun1 okudunuz sayfa sayısı "))
+gun4=int(input("gun1 okudunuz sayfa sayısı "))
+gun5=int(input("gun1 okudunuz sayfa sayısı "))
+gun6=int(input("gun1 okudunuz sayfa sayısı "))
+gun7=int(input("gun1 okudunuz sayfa sayısı "))
+
+toplam=gun1+gun2+gun3+gun4+gun5+gun6+gun7
+print("1hadtada kudunuz kitap sayısı",toplam)
