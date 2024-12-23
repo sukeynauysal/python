594ab1460defaa713b688490b5f56113d77e8dc9
sukeynauysal <sukeynauysal05@gmail.com>
2024-12-23 11:45:43 +0300

    Add files via upload

---
+++ ornek2.py
@@ -0,0 +1,7 @@
+yazili1=int(input("1.yaziliyi girin"))
+yazili2=int(input("2.yaziliyi giriniz"))
+
+performans1=int(input("1.performansi giriniz"))
+performans2=int(input("2.performansi giriniz"))
+ort_not=(yazili1+yazili2+performans1+performans2)/2
+print("yazılıların toplamının ortalaması", ort_not)
---
+++ ornek4.py
@@ -0,0 +1,4 @@
+yaricap=int(input("yarı çap giriniz : "))
+pi=3.14
+alan=pi*yaricap*yaricap
+print("dairenin alanı :", alan)
